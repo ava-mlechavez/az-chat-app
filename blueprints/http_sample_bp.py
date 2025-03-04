5080ff670cede12fede9a3b4749522e4cd516c58
Mark Lester Echavez <marklester.echavez@avanade.com>
2025-03-04 12:54:06 -0500

    refactor: change the sample route to support fast api

--- blueprints/http_sample_bp.py
+++ blueprints/http_sample_bp.py
@@ -1,5 +1,7 @@
+import os
+import json
 import azure.functions as func
-from azurefunctions.extensions.http.fastapi import Request, StreamingResponse
+from azurefunctions.extensions.http.fastapi import Request, Response
 import openai
 from azure.identity import DefaultAzureCredential, get_bearer_token_provider
 
@@ -8,16 +10,24 @@ sample_bp = func.Blueprint()
 @sample_bp.route(
     route="sample", methods=[func.HttpMethod.GET], auth_level=func.AuthLevel.ANONYMOUS
 )
-async def sample(req: func.http.HttpRequest):
+async def sample(req: Request):
     client = openai.AsyncAzureOpenAI(
         azure_endpoint=openai.azure_endpoint,
-        azure_ad_token_provider= get_bearer_token_provider(DefaultAzureCredential()),
+        azure_ad_token_provider= get_bearer_token_provider(
+            DefaultAzureCredential(),
+            "https://cognitiveservices.azure.com/.default"),
         api_version=os.environ["OPENAI_API_VERSION"]
     )
 
     completion = await client.chat.completions.create(
-        model="gpt-4o",
+        model="gpt-4o-mini",
         messages=[
             {"role": "user", "content": "Say this is a test."},
         ],
+    )
+
+    return Response(
+        content=json.dumps({ "message": completion.choices[0].message.content}),
+        status_code=200,
+        media_type="application/json"
     )
