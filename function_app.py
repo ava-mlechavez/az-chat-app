68667270be6abeef4c9bff5afdb9e144fa6f43fa
Mark Lester Echavez <marklester.echavez@avanade.com>
2025-03-04 12:55:07 -0500

    refactor: register functions clean up

--- function_app.py
+++ function_app.py
@@ -1,8 +1,7 @@
 import azure.functions as func
 from blueprints.http_chat_blueprint import chat_bp
-from blueprints.http_search_bp import simple_search_bp
+from azurefunctions.extensions.http.fastapi import Request, JSONResponse
 
 app = func.FunctionApp()
 
-app.register_blueprint(chat_bp)
-# app.register_blueprint(simple_search_bp)
+app.register_functions(chat_bp)
