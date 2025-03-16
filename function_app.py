7513731384b2149d9f80c21147d064fbb661ffe8
Mark Lester Echavez <marklester.echavez@avanade.com>
2025-03-15 23:15:56 -0400

    refactor: remove the http chat route

--- function_app.py
+++ function_app.py
@@ -1,8 +1,6 @@
 import azure.functions as func
-from blueprints.http_chat_blueprint import chat_bp
 from blueprints.http_semantic_kernel_bp import bp as semantic_kernel_bp
 
 app = func.FunctionApp()
 
-app.register_functions(chat_bp)
 app.register_functions(semantic_kernel_bp)
